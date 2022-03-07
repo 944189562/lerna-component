7e0c15fe51201d29caf50cd79f5ed4624c544b32
Justin <944189562@qq.com>
2022-03-07 10:09:42 +0800

    init

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: ["../packages/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+};
---
+++ packages/input/stories/input.stories.js
@@ -0,0 +1,31 @@
+import LgInput from '../'
+
+export default {
+  title: 'LgInput',
+  component: LgInput,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
+    onClick: {},
+  },
+}
+
+export const Text = () => ({
+  components: { LgInput },
+  template: '<lg-input v-model="value"></lg-input>',
+  data () {
+    return {
+      value: 'admin'
+    }
+  }
+})
+
+export const Password = () => ({
+  components: { LgInput },
+  template: '<lg-input type="password" v-model="value"></lg-input>',
+  data () {
+    return {
+      value: 'admin'
+    }
+  }
+})
